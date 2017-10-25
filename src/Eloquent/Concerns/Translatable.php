34cdf42d33ef86b7cb44346eb562734e862ac169
Shangin Nikolai <shanginn@gmail.com>
2017-10-25 18:10:13 +0500

    fix getDirty

--- src/Eloquent/Concerns/Translatable.php
+++ src/Eloquent/Concerns/Translatable.php
@@ -98,9 +98,9 @@ trait Translatable
      *
      * @return array
      */
-    public function getDirty()
+    public function getDirtyTranslations()
     {
-        $dirty = parent::getDirty();
+        $dirty = [];
 
         $dirtyTranslations = $this->translations->reduce(function ($array, Model $translation) {
             $array[$translation->locale] = $translation->getDirty();
@@ -117,6 +117,38 @@ trait Translatable
         }
 
         return $dirty;
+    }
+
+    /**
+     * Determine if the model or given attribute(s) have been modified.
+     *
+     * @param  array|string|null  $attributes
+     * @return bool
+     */
+    public function isDirty($attributes = null)
+    {
+        $dirty = array_merge($this->getDirty(), $this->getDirtyTranslations());
+
+        // If no specific attributes were provided, we will just see if the dirty array
+        // already contains any attributes. If it does we will just return that this
+        // count is greater than zero. Else, we need to check specific attributes.
+        if (is_null($attributes)) {
+            return count($dirty) > 0;
+        }
+
+        $attributes = is_array($attributes)
+            ? $attributes : func_get_args();
+
+        // Here we will spin through every attribute and see if this is in the array of
+        // dirty attributes. If it is, we will return true and if we make it through
+        // all of the attributes for the entire array we will return false at end.
+        foreach ($attributes as $attribute) {
+            if (array_key_exists($attribute, $dirty)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
